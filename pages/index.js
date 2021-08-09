d749ecce1841c02a050f3478489a257d53ebcbf7
Sean <sean@vestaridefair.com>
2021-08-09 12:56:33 -0700

    learning udemy

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import PageContainer from 'components/PageContainer';
 import Actions from 'components/Actions';
 import styled from 'styled-components';
 import { MyButton, MyDarkModeSwitch } from 'styles/theme';
+import { Box, Button, VStack } from '@chakra-ui/react';
+
+const VisitEvents = () => (
+  <Link href="/myindex">
+    <a>
+      <MyButton>Visit The Events App!</MyButton>
+    </a>
+  </Link>
+);
 
 export default function Home() {
   return (
-    <PageContainer title="Next.js Chakra Starter">
-      <Actions switchName="Second Page" />
-
-      <MyButton>Me</MyButton>
-
-    </PageContainer>
+    <>
+      <PageContainer title="Next.js Chakra Starter">
+        <VisitEvents />
+        <Actions switchName="Second Page" />
+      </PageContainer>
+    </>
   );
 }
